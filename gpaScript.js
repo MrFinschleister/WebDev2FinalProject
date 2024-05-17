583aef556125b43798dc533e5896e61f77b58013
sammyfriedland <147730455+sammyfriedland@users.noreply.github.com>
2024-05-17 09:13:03 -0500

    Add files via upload

---
+++ gpaScript.js
@@ -0,0 +1,70 @@
+var cOne;
+function cone(classOneText){
+    cOne = parseInt(classOneText);
+};
+var cTwo;
+function ctwo(classTwoText){
+    cTwo = parseInt(classTwoText);
+};
+var cThree;
+function cthree(classThreeText){
+    cThree = parseInt(classThreeText);
+};
+var cFour;
+function cfour(classFourText){
+    cFour = parseInt(classFourText);
+};
+var cFive;
+function cfive(classFiveText){
+    cFive = parseInt(classFiveText);
+};
+var cSix;
+function csix(classSixText){
+    cSix = parseInt(classSixText);
+};
+var cSeven;
+function cseven(classSevenText){
+    cSeven = parseInt(classSevenText);
+};
+var cEight;
+function ceight(classEightText){
+    cEight = parseInt(classEightText);
+};
+var oneType;
+function tOne(classOneType){
+    oneType = classOneType
+}
+var twoType;
+function tTwo(classTwoType){
+    twoType = classTwoType
+}
+var threeType;
+function tThree(classThreeType){
+    threeType = classThreeType
+}
+var fourType;
+function tFour(classFourType){
+    fourType = classFourType
+}
+var fiveType;
+function tFive(classFiveType){
+    fiveType = classFiveType
+}
+var sixType;
+function tSix(classSixType){
+    sixType = classSixType
+}
+var sevenType;
+function tSeven(classSevenType){
+    sevenType = classSevenType
+}
+var eightType;
+function tEight(classEightType){
+    eightType = classEightType
+}
+
+function submit(oneType,twoType,threeType,fourType,fiveType,sixType,sevenType,eightType){
+    if (oneType = "AP"){
+        
+    }
+}
